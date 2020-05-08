3f52768a45b4d86c3fae0f57dfe94f6c0960e5fb
LeaSuccFree <papanb.dev@gmail.com>
2020-05-08 20:27:29 +0200

    Updates

---
+++ precache-manifest.fe7c345767db98c96ebf52fecfbf2bee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4da118d9b42f9857c0763b1cd448a0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1be9bad8c90286e4b5f1",
+    "url": "/static/css/main.22b42a43.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be9bad8c90286e4b5f1",
+    "url": "/static/js/main.a2f772b2.chunk.js"
+  },
+  {
+    "revision": "7129a994307f4aa325e4",
+    "url": "/static/js/runtime-main.c8f32266.js"
+  }
+]);
