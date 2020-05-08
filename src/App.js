b71dce2b054eff59a224dcdbad7d72493037bf6e
LeaSuccFree <papanb.dev@gmail.com>
2020-05-08 19:06:25 +0200

    Changed

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+
+import {CardList} from './Components/card-list/card-list.components'
+import {SearchBox} from './Components/search-box/search-box.component'
+
+import './App.css';
+
+class App extends Component{
+  constructor(){
+    super();//Calls the constructor method on the component class ==> This gave us access to this.state
+    this.state = {
+      monsters:[],
+      searchField: ''
+    }
+  }
+
+ componentDidMount(){
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
+  }
+
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value})
+  }
+
+  render(){
+    const { monsters, searchField } = this.state; // destructuring
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+    return (
+    <div className="App">
+    <SearchBox
+      placeholder='SearchMonsters'
+      handleChange={this.handleChange}
+    />
+    <CardList monsters={filteredMonsters}/>
+    </div>
+    )
+  }
+}
+
+export default App;
